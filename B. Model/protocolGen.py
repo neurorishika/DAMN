eed5108303884363aa4027d4eb0282b9a918fb34
Saptarshi <saptarshi.mohanta@gmail.com>
2019-08-23 18:44:08 +0530

    Fourth Commit

--- B. Model/protocolGen.py
+++ B. Model/protocolGen.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pickle
+import easygui
 
 data = {}
 data['duration'] = 6000
@@ -11,6 +12,6 @@ data['pulse_duration'] = 1000
 data['pulse_delay'] = 500
 data['odors'] = ['0_1']
 
-protocol_path = easygui.filesavebox(msg='Save Protocol File',title='Protocol Browser',default='{}.locust'.format(dt.strftime("Locust_%d%m%Y_%H%M")),filetypes=['*.locust'])
+protocol_path = easygui.filesavebox(msg='Save Protocol File',title='Protocol Browser',default='Dur_{}_Pulse_{}_Odor_{}.protocol'.format(data['duration'],data['pulse_duration'],data['odors']),filetypes=['*.protocol'])
 with open(protocol_path, 'wb') as fp:
     pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
